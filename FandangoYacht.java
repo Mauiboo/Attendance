c3aa77e3113fca1f93167e0a40cb0707931b771e
Mauiboo <132040849+Mauiboo@users.noreply.github.com>
2023-05-27 19:40:49 +0800

    Add files via upload

---
+++ FandangoYacht.java
@@ -0,0 +1,17 @@
+package IndividualActivity;
+
+public class FandangoYacht extends Vehicle {
+
+String MainSailColor = "Blue";
+	
+	//Method for float
+	public void Float() {
+		System.out.println("The boat is sailing.");
+	}
+		
+	@Override
+	public void stop() {
+		super.stop();
+	}
+}
+
